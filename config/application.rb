999ea03ae221bc9bb904e86375373d09dd4358bf
LockeZz <LockeZz.PHF@gmail.com>
2021-07-15 00:58:12 +0800

    added environment varialbe settings

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module FacebookObs
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config_file = Rails.application.config_for(:application) 
+    config_file.each do |key, value| 
+      ENV["#{key}"] = value
+    end unless config_file.nil?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_13_165700) do
+ActiveRecord::Schema.define(version: 2021_07_13_174307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
